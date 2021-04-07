62e75a9ba8b91dce717cf76cebcecae4b357a013
helmik2 <helmi_kindy@protonmail.com>
2021-04-07 13:36:25 +0300

    01 completed

--- python_fundamentals-master/01_python_fundamentals/01_03_yeehaw.py
+++ python_fundamentals-master/01_python_fundamentals/01_03_yeehaw.py
@@ -4,5 +4,8 @@ Write the necessary code to display the follow message to the console
 	I'm a programmer now.
 	Yeehaw!
 	Coding here I come!
-
 '''
+
+print("I'm a programmer now.")
+print("Yeehaw!")
+print("Coding here I come!")
